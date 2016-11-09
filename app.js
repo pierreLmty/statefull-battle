b24d712863f916fbf1870494a71338f51b0fc02f
pierrelmty <e127718z@etu.univ-nantes.fr>
2016-11-09 03:51:02 +0100

    Ajout des transitions entre les pages + ajout de méthodes de l'api

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-var battlemind = angular.module('battlemind', ['angular-google-gapi']);
+var battlemind = angular.module('battlemind', []);
 
-battlemind.controller('AppController', ['$scope', 'GApi', function($scope, GApi) {
+battlemind.controller('AppController', ['$scope', '$window', function($scope, $window) {
 
 	$scope.page = null;
 	$scope.highscores = null;
 	
+	var rootApi = 'https://1-dot-statefull-battle.appspot.com/_ah/api/';
+	
+	//initialisation de la liaison entre l'api et l'interface
+	$window.init = function() {
+		console.log("windowinit called");
+		
+		console.log("Chargement de l'API");
+		
+		var functionLoadQuestion = function() {
+						console.log("Chargement des questions et des réponses");
+						var req = gapi.client.questionentityendpoint.listQuestionEntity();
+						req.execute(
+							function(resp) {
+								$scope.listQuestions = resp.items;
+								$scope.$apply();
+								console.log(resp);
+							}
+						);
+					}
+					
+		gapi.client.load('questionentityendpoint', 'v1', functionLoadQuestion, rootApi);
+	}
+	
+	//Sélection de la page
 	$scope.selectPage = function(page){
 		$scope.page = page;
 
@@ -15,7 +39,19 @@ battlemind.controller('AppController', ['$scope', 'GApi', function($scope, GApi)
 			
 		}
 		else if (page == 'highscores'){
-			
+			var functionScore = function() {
+						console.log("score api loaded");
+						var req = gapi.client.questionentityendpoint.listScoreEntity();
+						req.execute(
+							function(resp) {
+								$scope.scores=resp.items;
+								$scope.$apply();
+								console.log(resp);
+							}
+						);
+					}
+		
+			gapi.client.load('questionentityendpoint', 'v1', functionScore, rootApi);
 		}
 	}
 	
